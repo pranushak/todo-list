662862995a9a5d2ce8d080ca762945439a6c27b9
Pranusha Keerthipati <pranushak87@gmail.com>
2026-04-14 20:10:32 -0500

    remove duplicate todoList array

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import TodoForm from './components/TodoForm'
 
 function App() {
 
-  const todoList = [
-    { id: 1, title: "review resources" },
-    { id: 2, title: "take notes" },
-    { id: 3, title: "code out app" }
-  ]
-
   return (
     <div>
     <h1>Todo List</h1>
